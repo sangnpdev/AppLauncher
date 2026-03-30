c937632bd5549778bc42bd5636981b8840dcae67
sangnpdev <sangnguyen20050916@gmail.com>
2026-03-30 15:16:21 +0700

    feat: Refactor shortcut creation script to use dynamic paths for executable and icon

--- gensc.ps1
+++ gensc.ps1
@@ -1,23 +1,32 @@
-$exePath = 'D:\Program\AppLauncher\bin\Debug\net10.0-windows\AppLauncher.exe'
+$scriptDirectory = Split-Path -Path $MyInvocation.MyCommand.Path -Parent
+$exePath = Join-Path -Path $scriptDirectory -ChildPath 'bin\Debug\net10.0-windows\AppLauncher.exe'
+$iconPath = Join-Path -Path $scriptDirectory -ChildPath 'image\icon-app.ico'
 
 if (-not (Test-Path -LiteralPath $exePath)) {
     throw "Executable not found: $exePath"
 }
 
-$shortcutDirectory = Split-Path -Path $exePath -Parent
+$desktopDirectory = [Environment]::GetFolderPath('DesktopDirectory')
 $shortcutName = '{0}.lnk' -f [System.IO.Path]::GetFileNameWithoutExtension($exePath)
-$shortcutPath = Join-Path -Path $shortcutDirectory -ChildPath $shortcutName
+$shortcutPath = Join-Path -Path $desktopDirectory -ChildPath $shortcutName
+$workingDirectory = Split-Path -Path $exePath -Parent
 
 if (Test-Path -LiteralPath $shortcutPath) {
     Remove-Item -LiteralPath $shortcutPath -Force
 }
 
+$iconLocation = if (Test-Path -LiteralPath $iconPath) {
+    '{0},0' -f $iconPath
+} else {
+    '{0},0' -f $exePath
+}
+
 $shell = New-Object -ComObject WScript.Shell
 $shortcut = $shell.CreateShortcut($shortcutPath)
 $shortcut.TargetPath = $exePath
-$shortcut.WorkingDirectory = $shortcutDirectory
-$shortcut.IconLocation = $exePath
+$shortcut.WorkingDirectory = $workingDirectory
+$shortcut.IconLocation = $iconLocation
 $shortcut.Description = 'AppLauncher'
 $shortcut.Save()
 
-Write-Host "Shortcut created: $shortcutPath"
+Write-Host "Shortcut created on Desktop: $shortcutPath"
